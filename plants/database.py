6a96ec9951652f629bed990db289b3272e702bda
Henrique <henriqmayer@gmail.com>
2021-07-18 22:07:22 -0300

    add postgree to production

--- plants/database.py
+++ plants/database.py
@@ -1,11 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+
+DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///./sql_app.db")
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}  # check_same_thread is used only for sqlite.
+    DATABASE_URL, connect_args={"check_same_thread": False}  # check_same_thread is used only for sqlite.
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
